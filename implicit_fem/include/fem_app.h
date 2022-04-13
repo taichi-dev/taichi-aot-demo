62be87df86b72518bd95691a048fa9302e804482
Ailing <ailzhang@users.noreply.github.com>
2022-04-13 13:06:18 +0800

    Update camera position (#15)

--- implicit_fem/include/fem_app.h
+++ implicit_fem/include/fem_app.h
@@ -449,7 +449,7 @@ class FemApp {
         glm::radians(55.0f), float(width_) / float(height_), 0.1f, 10.0f);
     constants->proj[1][1] *= -1.0f;
 #ifdef ANDROID
-    constexpr float kCameraZ = 4.8f;
+    constexpr float kCameraZ = 4.85f;
 #else
     constexpr float kCameraZ = 4.8f;
 #endif
