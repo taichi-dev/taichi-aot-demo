9c3566d7af29a5880c6b8e8ccb86dcdb605af1fc
Ailing <ailzhang@users.noreply.github.com>
2022-04-13 12:01:55 +0800

    Hide title bar (#13)

--- implicit_fem/android/app/src/main/java/com/taichigraphics/aot_demos/implicit_fem/MainActivity.java
+++ implicit_fem/android/app/src/main/java/com/taichigraphics/aot_demos/implicit_fem/MainActivity.java
@@ -1,6 +1,9 @@
 package com.taichigraphics.aot_demos.implicit_fem;
 
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 
@@ -10,6 +13,11 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        try
+        {
+            this.getSupportActionBar().hide();
+        }
+        catch (NullPointerException e){}
         vkSurfaceView = new VKSurfaceView(this);
         setContentView(vkSurfaceView);
     }
