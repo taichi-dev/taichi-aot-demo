20951e0df2e26b5fc9204ef3a513b2f99eea3f0f
Zhanlue Yang <jim19930609@gmail.com>
2022-08-05 11:51:01 +0800

    Resolved issues with mpm88 demo - vulkan backend (#27)
    * Added taichi_sparse demo
    
    * update
    
    * Complete TaichiSparse Demo
    
    * Cleaned up Sparse Taichi Demo
    
    * Added shader files
    
    * Mpm88 example
    
    * Fixed GUI issue with taichi_sparse
    
    * mpm88 with c-api
    
    * Fixed typo
    
    * Finalize two demos
    
    * Fixed type
    
    * Fixed issues with mpm88 demo - vulkan backend
    
    * fix typo

--- mpm88_desktop/linux_compile_and_run.sh
+++ mpm88_desktop/linux_compile_and_run.sh
@@ -1,8 +1,11 @@
-BACKEND_NAME="cuda" # cuda, x64, vulkan
-TAICHI_REPO="/home/taichigraphics/workspace/taichi"
+BACKEND_NAME="vulkan" # cuda, x64, vulkan
+TAICHI_REPO="/home/taichigraphics/workspace/taichi2"
 
 AOT_DIRECTORY="/tmp/aot_files"
 RUNTIME_LIB="${TAICHI_REPO}/python/taichi/_lib/runtime"
+
+rm -rf ${AOT_DIRECTORY}
+mkdir -p ${AOT_DIRECTORY}
 
 rm -rf build && mkdir build && cd build
 cmake .. -DTAICHI_REPO=${TAICHI_REPO} && make -j && cd ..
