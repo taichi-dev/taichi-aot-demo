29647d9b5915bad34be955e7400f7a293f9dfbeb
jim19930609 <jim19930609@gmail.com>
2022-08-09 10:01:32 +0800

    Update README

--- taichi_sparse/linux_compile_and_run.sh
+++ taichi_sparse/linux_compile_and_run.sh
@@ -4,6 +4,9 @@ TAICHI_REPO="/home/taichigraphics/workspace/taichi"
 AOT_DIRECTORY="/tmp/aot_files"
 RUNTIME_LIB="${TAICHI_REPO}/python/taichi/_lib/runtime"
 
+rm -rf ${AOT_DIRECTORY}
+mkdir -p ${AOT_DIRECTORY}
+
 rm -rf build && mkdir build && cd build
 cmake .. -DTAICHI_REPO=${TAICHI_REPO} && make -j && cd ..
 
