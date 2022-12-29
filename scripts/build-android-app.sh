196f4a9daa5dbbde0a63fb40ccb01561efca79b8
Zhanlue Yang <jim19930609@gmail.com>
2022-12-29 15:17:33 +0800

    Fix build scripts for Android App (#104)

--- scripts/build-android-app.sh
+++ scripts/build-android-app.sh
@@ -15,9 +15,6 @@ echo "sdk.dir=$ANDROID_SDK_ROOT" >> local.properties
 java \
     "-DlibName=$1" \
     "-Dorg.gradle.appname=$1" \
-    "-Dhttps.proxyHost=127.0.0.1" \
-    "-Dhttps.proxyPort=1080" \
-    "-Dhttp.nonProxyHosts=*.nonproxyrepos.com|localhost" \
     -classpath "gradle/wrapper/gradle-wrapper.jar" \
     org.gradle.wrapper.GradleWrapperMain build
 
--- scripts/build-android.sh
+++ scripts/build-android.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -e
 
-#rm -rf build-android-aarch64
-#mkdir build-android-aarch64
+rm -rf build-android-aarch64
+mkdir build-android-aarch64
 pushd build-android-aarch64
 TAICHI_C_API_INSTALL_DIR="${PWD}/../build-taichi-android-aarch64/install/c_api" cmake .. \
     -DCMAKE_TOOLCHAIN_FILE="$ANDROID_NDK_ROOT/build/cmake/android.toolchain.cmake" \
