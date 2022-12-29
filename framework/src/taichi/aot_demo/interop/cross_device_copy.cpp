4a6355dfd60656180c0bea669ba6ee72f83315ef
damnkk <17611288725@163.com>
2022-12-29 14:59:02 +0800

    step

--- framework/src/taichi/aot_demo/interop/cross_device_copy.cpp
+++ framework/src/taichi/aot_demo/interop/cross_device_copy.cpp
@@ -1,6 +1,6 @@
 #include "taichi/aot_demo/interop/cross_device_copy.hpp"
 #include "taichi/aot_demo/interop/common_utils.hpp"
-#include "taichi/aot_demo/renderer.hpp"
+#include "taichi/aot_demo/renderer.hpp" 
 
 
 namespace ti {
